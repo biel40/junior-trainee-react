2fad05b607462156ecaa0eb6b27b09a2e98b054a
Gabriel Borrás Serra <biel40@Air-de-Gabriel.home>
2023-01-26 13:25:05 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Prueba técnica para juniors & Trainees de React
+
+APIs:
+- Facts random: https://catfact.ninja/fact
+- Imagen random: https://cataas.com/cat/says/${firstWord}
+
+- Recuperar un "Fact" de gatos aleatorio de la primera API
+- Recuperar la primera palabra del "Fact"
+- Muestra una imagen del gato con la primera palabra del "Fact" que recuperes.
---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+
+function App () {
+  const [fact, setFact] = useState()
+  const [imgUrl, setImgUrl] = useState()
+
+  useEffect(() => {
+    async function getRandomFact () {
+      const res = await fetch(CAT_ENDPOINT_RANDOM_FACT)
+      const json = await res.json()
+      const currectFact = json.fact
+      setFact(currectFact)
+      const firstWord = currectFact.split(' ')[0]
+      const res2 = await fetch(`https://cataas.com/cat/says/${firstWord}`)
+      setImgUrl(await res2.url)
+    }
+
+    getRandomFact()
+  }, [])
+
+  return (
+    <div className='App'>
+      <h1>App de gatitos</h1>
+      <p>{fact}</p>
+      <img src={imgUrl} alt='cat' />
+    </div>
+  )
+}
+
+export default App
